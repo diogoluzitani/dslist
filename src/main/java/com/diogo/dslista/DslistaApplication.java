8223ca9deed29c848aca804468277251270428fe
Diogo Luzitani Fernandes da Silva <1642@in.cassems.com.br>
2023-07-12 22:23:10 -0400

    Project Create

---
+++ src/main/java/com/diogo/dslista/DslistaApplication.java
@@ -0,0 +1,13 @@
+package com.diogo.dslista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistaApplication.class, args);
+	}
+
+}
